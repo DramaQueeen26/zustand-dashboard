f41fa3b51de7f8bb3b86986f5c86c26ee303b342
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-10-20 14:00:58 -0400

    Consumir store

--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -5,11 +5,15 @@ interface BearState {
     polarBears: number
     pandaBears: number
     increaseBlackBears: (by: number) => void
+    increasePolarBears: (by: number) => void
+    increasePandaBears: (by: number) => void
 }
 
 export const useBearStore = create<BearState>()((set) => ({
   blackBears: 10,
   polarBears: 5,
   pandaBears: 1,
-  increaseBlackBears: (by: number) => set( (state) => ({ blackBears: state.blackBears + by }) )
+  increaseBlackBears: (by: number) => set( (state) => ({ blackBears: state.blackBears + by }) ),
+  increasePolarBears: (by: number) => set( (state) => ({ polarBears: state.polarBears + by }) ),
+  increasePandaBears: (by: number) => set( (state) => ({ pandaBears: state.pandaBears + by }) )
 }))
